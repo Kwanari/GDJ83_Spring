ac52ebea97641895c007648dd27dd763d07a8395
Kwan <kwanda1227@gmail.com>
2024-07-03 23:02:17 +0900

    home bank list

--- src/main/java/com/kwan/app/product/ProductDAO.java
+++ src/main/java/com/kwan/app/product/ProductDAO.java
@@ -3,48 +3,24 @@ package com.kwan.app.product;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-
-import com.kwan.app.util.DBConnection;
 
 @Repository
 public class ProductDAO {
 
 	@Autowired
-	private DBConnection dbConnection;
+	SqlSession sqlSession;
+
+	private final String NAMESPACE = "com.kwan.app.product.ProductDAO.";
 
 	public List<ProductDTO> getList() throws Exception {
 
-		Connection con = dbConnection.getConnection();
+		return sqlSession.selectList(NAMESPACE + "getList");
 
-		String sql = "SELECT * FROM ITEMS ORDER BY ITEM_ID ASC";
-
-		PreparedStatement st = con.prepareStatement(sql);
-
-		ResultSet rs = st.executeQuery();
-
-		ArrayList<ProductDTO> list = new ArrayList<ProductDTO>();
-
-		while (rs.next()) {
-			ProductDTO productDTO = new ProductDTO();
-
-			productDTO.setItem_id(rs.getLong("ITEM_ID"));
-			productDTO.setItem_name(rs.getString("ITEM_NAME"));
-			productDTO.setItem_detail(rs.getString("ITEM_DETAIL"));
-			productDTO.setItem_rate(rs.getDouble("ITEM_RATE"));
-
-			list.add(productDTO);
-		}
-
-		rs.close();
-		st.close();
-		con.close();
-
-		return list;
 	}
 
 	public ProductDTO getDetail(ProductDTO productDTO) throws Exception {
